3f9d8a06d8531b76e17d05a39084506b112379e4
Psilo <psilo@unterrainer.info>
2020-09-15 14:00:20 +0200

    update

--- set_deployment_env.sh
+++ set_deployment_env.sh
@@ -2,5 +2,7 @@
 
 ## This file will be used in the docker-compose.yml file automatically because of its name and location.
 ## So this is the place where to transfer the CI-variables to docker-compose.
+echo "DB_PASSWORD=$DB_PASSWORD" >> .env
+echo "DB_ROOT_PASSWORD=$DB_ROOT_PASSWORD" >> .env
 echo "NEXUS_SMTP_USER=$NEXUS_SMTP_USER" >> .env
 echo "NEXUS_SMTP_PASSWORD=$NEXUS_SMTP_PASSWORD" >> .env
