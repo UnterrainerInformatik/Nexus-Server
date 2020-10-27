f48bb8ecb9cf5b06f8f26952693ca1c546894209
Psilo <psilo@unterrainer.info>
2020-10-27 09:32:43 +0100

    version bump

--- src/main/java/info/unterrainer/nexus/nexusserver/Information.java
+++ src/main/java/info/unterrainer/nexus/nexusserver/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.nexus.nexusserver;
 
 public class Information {
 	public static final String name = "Nexus-Server";
-	public static final String buildTime = "2020-10-23T13:30:59Z";
-	public static final String pomVersion = "0.1.0";
+	public static final String buildTime = "2020-10-27T08:32:20Z";
+	public static final String pomVersion = "0.1.1";
 }
